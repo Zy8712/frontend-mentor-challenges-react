f8879b1aa3263a5e4dd2bb6840dec533da6ae5fd
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2023-08-31 05:17:53 +0000

    added button hover to single price grid, installed dependencies for newsletter signup

--- single-price-grid-component/my-react-work-v1/src/BottomLeftSection.jsx
+++ single-price-grid-component/my-react-work-v1/src/BottomLeftSection.jsx
@@ -8,7 +8,7 @@ function BottomLeftSection() {
                     <h3 className="text-lg font-bold text-white mb-6">Monthly Subscription</h3>
                     <p className="text-base font-bold text-theme-light-gray mb-1"><span className="text-3xl font-bold text-white mr-1">$29</span>per month</p>
                     <p className="text-white mb-8">Full access for less than $1 a day</p>
-                    <button className="bg-theme-bright-yellow text-white font-bold px-[100px] py-3 rounded shadow-lg">Sign Up</button>
+                    <button className="bg-theme-bright-yellow text-white hover:bg-theme-light-gray hover:text-theme-bright-yellow font-bold px-[100px] py-3 rounded shadow-lg">Sign Up</button>
                 </div>
             </div>
         </>
